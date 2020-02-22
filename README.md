9fa69d16b07f2227662098c503acc4e99acf011c
Ahmet Yılmaz <ahmet.yilmaz@metglobal.com>
2020-02-22 23:12:25 +0300

    Memory Changer Developed

---
+++ README.md
@@ -0,0 +1,14 @@
+# Process Memory Changer
+_Making another process memory address access on Mac with ANSI C_
+
+## Building
+`gcc -Wall main.c lib/*/*.c -o mem-changer.o`
+
+## Execute
+The program runs in super user (sudo) mode
+`sudo ./mem-changer.o`
+###
+
+
+
+
---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "lib/xnumem/xnumem.h"
+
+int main(int argc, const char *argv[]) {
+    size_t size = sizeof(int);  // data size
+    pid_t pid;
+    long int address;
+    unsigned char *currentData, *newData;
+
+    printf("---[ Processs Memory Changer ]---\n");
+
+    printf("Enter PID :");
+    scanf("%d", &pid);
+    printf("[%d] PID selected\n", pid);
+
+    printf("Enter memory address :");
+    scanf("%ld", &address);
+    printf("[%ld] Memory address selected\n", address);
+
+    currentData = xnu_read(pid, (int *const) address, &size);
+    printf("Current Value :%i\n", (int) *currentData);
+
+    newData = (unsigned char *) malloc(size);
+    printf("Enter new value :");
+    scanf("%d", newData);
+
+    xnu_write(pid, (int *const) address, newData, size);
+
+    currentData = xnu_read(pid, (int *const) address, &size);
+    printf("%i\n", (int) *currentData);
+
+    getchar();
+
+    return 0;
+}
